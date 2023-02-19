67e77e2f7cdabc4c7ab21d02b0af19a4a3def365
P C Kalyan <pckalyan008@gmail.com>
2023-02-19 20:05:09 +0530

    added routerModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { NotebookComponent } from './notes/notebook/notebook.component';
 import { NotesModule } from './notes/notes.module';
 import { SharedModule } from './shared/shared.module';
+
+const appRoutes: Routes = [
+  { path: 'header', component: HeaderComponent },
+  { path: 'notebook', component: NotebookComponent }
+]
 
 @NgModule({
   declarations: [
@@ -13,7 +20,8 @@ import { SharedModule } from './shared/shared.module';
   ],
   imports: [
     BrowserModule,
-    NotesModule
+    NotesModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
